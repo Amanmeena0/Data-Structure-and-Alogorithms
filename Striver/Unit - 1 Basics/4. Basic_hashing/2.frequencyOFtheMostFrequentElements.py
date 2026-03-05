81ce06c9a2d1d61661c1aedcf3a4633c4482a249
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-03-05 15:58:49 +0530

    message

---
+++ Striver/Unit - 1 Basics/4. Basic_hashing/2.frequencyOFtheMostFrequentElements.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        
+
+        return
+
+
+sol = Solution()
+
+nums = [1,2,4]
+k =  5
+
+
+result = sol.maxFrequency(nums,k)
+print(result)
