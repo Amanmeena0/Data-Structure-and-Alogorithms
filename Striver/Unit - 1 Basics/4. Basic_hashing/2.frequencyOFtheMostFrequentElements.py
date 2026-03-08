79b35fe2d7724202354c1658f146d3285a4c9381
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-03-08 23:49:06 +0530

    message

--- Striver/Unit - 1 Basics/4. Basic_hashing/2.frequencyOFtheMostFrequentElements.py
+++ Striver/Unit - 1 Basics/4. Basic_hashing/2.frequencyOFtheMostFrequentElements.py
@@ -2,16 +2,28 @@ from typing import List
 
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        
+        l, r = 0, 0
 
-        return
+        total, res = 0 ,0
 
+        while r < len(nums):
+            total += nums[r]
+
+            while nums[r]*(r - l + 1 )> total + k:
+                total -= nums[l]
+                l += 1
+
+            res = max(res, r- l + 1)
+
+            r += 1
+
+        return res
 
 sol = Solution()
 
-nums = [1,2,4]
+nums = [1,4,8,13]
 k =  5
 
 
 result = sol.maxFrequency(nums,k)
-print(result)
+print(result)                                                             
