7677db3a6e3dffc3562eb80587f58933c7a7bf51
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-03-26 23:01:12 +0530

    merge sort

--- Striver/Units - 2 Sortings/Sorting-2/mergeSort.py
+++ Striver/Units - 2 Sortings/Sorting-2/mergeSort.py
@@ -1,5 +1,42 @@
-class Solution:
-    def mergeSort(self, nums):
+from typing import List
 
-        return
+class Solution:
+    def mergeSort(self, nums) :
+        if len(nums) <=1:
+            return nums
+
+        mid = len(nums)// 2
+
+        leftHalf = nums[:mid]
+        rightHalf = nums[mid:]
+
+        sortedLeft = mergeSort(leftHalf)
+        sortedRight = mergeSort(rightHalf)
+
+        return merge(sortedLeft, sortedRight)                
     
+    def merge(self,left, right):
+        result = []
+        i = j = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[j])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+
+        result.extend(left[i:])
+        result.extend(right[j:])
+
+        return result
+        
+    
+sol = Solution()
+
+nums = [7,3,6,4]
+
+result = sol.mergeSort(nums)
+
+print(result)
