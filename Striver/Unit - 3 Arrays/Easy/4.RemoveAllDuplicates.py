2824980a4daa289a1ea386ff4e646e2a3cfe2f44
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-02 22:15:36 +0530

    commit

---
+++ Striver/Unit - 3 Arrays/Easy/4.RemoveAllDuplicates.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> bool:
+        if not nums:
+            return 0
+        
+        j = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                j += 1
+                nums[j] = nums[i]
+
+        return j + 1
+
+def main():
+
+    sol = Solution()
+    array = list(map(int, input("Enter numbers separated by spaces: ").split()))
+  
+    print(sol.removeDuplicates(array))
+
+if __name__ == "__main__":
+    main()
