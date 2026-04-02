02d3f4e916894f84ed4be5528570afdb63a27c1b
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-02 15:03:10 +0530

    commit

--- Striver/Unit - 3 Arrays/Easy/3.CheckIfArrayIsSorted.py
+++ Striver/Unit - 3 Arrays/Easy/3.CheckIfArrayIsSorted.py
@@ -4,8 +4,6 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
         count = 0
         n = len(nums)
-        
-
         for i in range(n):
 
             if nums[i] > nums[(i + 1) % n]:
