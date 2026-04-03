a74edd430643bbbd210d3817425b6b313e82f6d2
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-03 06:22:09 +0530

    commit

---
+++ Striver/Unit - 3 Arrays/Easy/5.RotateArrayByOne.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def Solve(self, nums: List[int]) -> bool:
+        
+        n = len(nums)
+
+        if n == 0:
+            return 
+        
+        temp = [0]*n
+
+        for i in range(1,n):
+
+            temp[i - 1] = nums[i]
+
+        temp[n -1 ]  =  nums[0]
+
+        for num in temp:
+            print(num, end=" ")
+
+        print()
+
+def main():
+
+    sol = Solution()
+    array = list(map(int, input("Enter numbers separated by spaces: ").split()))
+  
+    sol.Solve(array)
+
+if __name__ == "__main__":
+    main()
