63f4ed82dee6f6c79072ebe163e1880b7c34c54e
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-03 06:44:59 +0530

    commit

--- Striver/Unit - 3 Arrays/Easy/6.RotateArrayByK.py
+++ Striver/Unit - 3 Arrays/Easy/6.RotateArrayByK.py
@@ -1,18 +1,37 @@
 from typing import List
 
 class Solution:
-    def rotate(self, nums: List[int]) -> bool:
-        
-        
-        
-        return 1
 
+    #brute force approach 
+    def rotate(self, nums: List[int], k: int) -> None:
+
+        n = len(nums)
+        k %= n
+        for _ in range(k):
+            last = nums.pop()
+            nums.insert(0, last)
+
+    #optimized version
+    def rotate1(self, nums: List[int], k: int) -> None:    
+        
+        n = len(nums)
+        k %= n
+        
+        nums.reverse()
+        nums[0:k] = nums[0:k][::-1]
+        nums[k:n] = nums[k:n][::-1]
+            
+        
+    
 def main():
 
     sol = Solution()
     array = list(map(int, input("Enter numbers separated by spaces: ").split()))
-  
-    print(sol.rotate(array))
+    k = int(input("Enter the K: "))
+    sol.rotate1(array,k)
+    
+    for num in array:
+        print(num, end=" ")
 
 if __name__ == "__main__":
     main()
