f2ad0b56784a94e6688e5f34e661789f0d9b1c30
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-03 06:27:09 +0530

    commit

---
+++ Striver/Unit - 3 Arrays/Easy/6.RotateArrayByK.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int]) -> bool:
+        
+        
+        
+        return 1
+
+def main():
+
+    sol = Solution()
+    array = list(map(int, input("Enter numbers separated by spaces: ").split()))
+  
+    print(sol.rotate(array))
+
+if __name__ == "__main__":
+    main()
