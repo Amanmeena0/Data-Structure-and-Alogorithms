e55b67c2cfb507967026d6f3fc0e8b67dc1cfbcf
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-04 17:58:16 +0530

    commit

---
+++ Striver/Unit - 3 Arrays/Easy/11.consecutiveOnes.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+
+    #brute force approach 
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        
+        MaxOnes = 0    
+        ones = 0
+        for i in range(len(nums)):
+
+            if(nums[i] == 1):
+                ones+=1
+            else:
+                MaxOnes =  max(MaxOnes, ones)
+                ones = 0
+
+        return MaxOnes
+            
+def main():
+
+    sol = Solution()
+    array = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+    print(sol.findMaxConsecutiveOnes(array))
+    
+
+if __name__ == "__main__":
+    main()
