4d891d25159bbe1f716b3b66247bf667e0d3d0bb
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-04 17:29:28 +0530

    commit

--- Striver/Unit - 3 Arrays/Easy/10.missingNumber.py
+++ Striver/Unit - 3 Arrays/Easy/10.missingNumber.py
@@ -3,7 +3,7 @@ from typing import List
 class Solution:
 
     #brute force approach 
-    def unionArray(self, nums: List[int]) -> int:
+    def missingNumber(self, nums: List[int]) -> int:
         missingNumber = 0
         n = len(nums)
         nums.sort()
@@ -20,8 +20,10 @@ def main():
     sol = Solution()
     array = list(map(int, input("Enter numbers separated by spaces: ").split()))
 
-    print(sol.unionArray(array))
+    print(sol.missingNumber(array))
     
 
 if __name__ == "__main__":
     main()
+
+
