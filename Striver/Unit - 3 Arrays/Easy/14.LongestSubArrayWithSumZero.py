8e0b1a631ec07950bc8546c8e0005879eb9041eb
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-05 13:05:44 +0700

    commit

--- Striver/Unit - 3 Arrays/Easy/14.LongestSubArrayWithSumZero.py
+++ Striver/Unit - 3 Arrays/Easy/14.LongestSubArrayWithSumZero.py
@@ -1,27 +1,25 @@
 from typing import List
 
 class Solution:
-    def longestSubarray(self, nums: List[int], k:int)-> int:
+    def longestSubarray(self, nums: List[int])-> int:
+        
         size = 0
-
         for i in range(len(nums)):
-
             sum = 0
+
             for j in range(i, len(nums)):
-                
                 sum += nums[j]
 
-                if sum == k:
+                if sum == 0:
                     size = max(size, j - i + 1)
-
+                    
         return size
      
 def main():
 
     sol = Solution()
     array = list(map(int, input("Enter numbers separated by spaces: ").split()))
-    k = int(input("Enter the number: "))
-    print(sol.longestSubarray(array,k))
+    print(sol.longestSubarray(array))
     
 
 if __name__ == "__main__":
