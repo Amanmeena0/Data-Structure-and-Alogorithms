c318b8597235fa69f9fca10f882d94057f685243
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-05 10:43:24 +0530

    commit

--- Striver/Unit - 3 Arrays/Easy/12.singleNumber.py
+++ Striver/Unit - 3 Arrays/Easy/12.singleNumber.py
@@ -1,25 +1,22 @@
 from typing import List
 from collections import Counter
-
 class Solution:
-
-   
     def singleNumber(self, nums: List[int]) -> int:
 
         freq = Counter(nums)    
-
         num = 0
+        number = len(nums)
 
+        if number == 1 :
+            return nums[0]   
         for key in freq:
 
-            number = len(nums)
 
-            if number > nums[key]:
-                number = key
-                num = nums[key]
+            if number > freq[key]:
+                number = freq[key]
+                num = key
 
         return num
-    
         
             
 def main():
@@ -27,7 +24,7 @@ def main():
     sol = Solution()
     array = list(map(int, input("Enter numbers separated by spaces: ").split()))
 
-    print(sol.findMaxConsecutiveOnes(array))
+    print(sol.singleNumber(array))
     
 
 if __name__ == "__main__":
