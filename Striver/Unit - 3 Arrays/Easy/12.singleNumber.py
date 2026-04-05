31f8b71e239f84c59a3b2d9cb42825e89673ab5e
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-05 10:52:53 +0530

    commit

--- Striver/Unit - 3 Arrays/Easy/12.singleNumber.py
+++ Striver/Unit - 3 Arrays/Easy/12.singleNumber.py
@@ -1,24 +1,38 @@
 from typing import List
 from collections import Counter
 class Solution:
+    
+    #brute force code
     def singleNumber(self, nums: List[int]) -> int:
-
         freq = Counter(nums)    
         num = 0
         number = len(nums)
-
         if number == 1 :
             return nums[0]   
         for key in freq:
-
-
             if number > freq[key]:
                 number = freq[key]
                 num = key
-
         return num
-        
-            
+    
+    #optimized code 
+    def singleNumber1(self, nums: List[int]) -> int:
+        freq = Counter(nums)
+
+        for key in freq:
+            if freq[key] == 1:
+                return key
+    
+
+    #Best approach for this xor approach
+    def singleNumber1(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+
+        return result
+
+
 def main():
 
     sol = Solution()
