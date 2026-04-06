62893abab25054a451a8ae34a8af87a31a11bd00
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-06 09:56:14 +0530

    commit

--- Striver/Unit - 3 Arrays/Hard/3.MajorityElement.py
+++ Striver/Unit - 3 Arrays/Hard/3.MajorityElement.py
@@ -5,14 +5,31 @@ class Solution:
     def sortColors(self, nums: List[int]) -> int:
         
         freq = Counter(nums)
+        n = len(nums)
+        n /= 2
 
+        for key in freq:
+            if freq[key] > n:
+                return key
 
+        return 
+
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = 0
         
-
-
-        return -1
-
+        for num in nums:
+            if count == 0:
+                candidate = num
+            
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
         
+        return candidate
+    
+
 def main():
 
     sol = Solution()
