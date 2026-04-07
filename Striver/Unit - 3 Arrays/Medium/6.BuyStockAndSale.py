7885efe11f86044dde35610e4aae9064499e9e53
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-07 09:15:50 +0530

    commit

--- Striver/Unit - 3 Arrays/Medium/6.BuyStockAndSale.py
+++ Striver/Unit - 3 Arrays/Medium/6.BuyStockAndSale.py
@@ -2,15 +2,19 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        
-        return 
+        profit = 0
+        for buy in range(len(prices)):
+            for sell in range(len(prices)):
+                if prices[buy] < prices[sell]:
+                    profit = max(profit, prices[sell] - prices[buy])
+        return profit 
         
 def main():
 
     sol = Solution()
     array = list(map(int, input("Enter numbers separated by spaces: ").split()))
     # k = int(input("Enter the number: "))
-    print(sol.sortColors(array))
+    print(sol.maxProfit(array))
     # sol.sortColors(array)
     # for i in array:
     #     print(i, end=" ")
