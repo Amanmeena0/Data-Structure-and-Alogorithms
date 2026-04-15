f806eb0cf5457ec31dbe1f4d788bf770680b71cc
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-16 05:15:52 +0530

    commit

--- Striver/Unit - 3 Arrays/Hard/5.MaximumSubarrayWithSum0.py
+++ Striver/Unit - 3 Arrays/Hard/5.MaximumSubarrayWithSum0.py
@@ -2,29 +2,44 @@ from typing import List
 
 
 class Solution:
-    def maxlen(self,arr:List[int])->int:
-        N = len(arr)
-        positives = []
-        negatives = []
+    def maxlen1(self,arr:List[int])->int:
+        n = len(arr)
+        max_ele = 0
+        for i in range(n):
+            total_sum = 0
+            for j in range(i, n):
 
-        for num in arr:
-            if num >= 0:
-                positives.append(num)
-            else:
-                num < 0:
-                negatives.append(num)
-        
+                total_sum += arr[j]
+                if total_sum == 0:
+                    max_ele = max(max_ele, j - i + 1)
 
+        return max_ele
+    
+    #using prefixSum 
+    def maxLen(self,arr: List[int])->int:
 
-        total_sum = sum(positives)
-        count = len(positives)
-        return
+        max_size = 0
+        prefix_sum = 0
+        freq = {}
+        freq[0] = -1
+
+        for i in range(len(arr)):
+            prefix_sum += arr[i]
+
+            if prefix_sum not in freq:
+                freq[prefix_sum] = i
+            
+            curr_size = i - freq[prefix_sum]
+
+            max_size = max(max_size, curr_size)
+
+        return max_size
 
 def main():
     sol = Solution()
     array = list(map(int, input("Enter numbers separated by spaces: ").split()))
 
-    print(sol.maxlen(array))
+    print(sol.maxLen(array))
 
     
 if __name__ == "__main__":
