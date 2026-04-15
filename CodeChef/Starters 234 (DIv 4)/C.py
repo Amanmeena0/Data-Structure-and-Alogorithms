b7a462f5217a4524246bdc510a6e5cec3c4671cf
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-15 21:49:59 +0530

    commit

---
+++ CodeChef/Starters 234 (DIv 4)/C.py
@@ -0,0 +1,37 @@
+def maximum_subsequence(Array):
+
+    positives = []
+    negatives = []
+
+    for num in Array:
+
+        if num >= 0:
+            positives.append(num)
+        else:
+            negatives.append(num)
+
+    total_sum = sum(positives)
+    count = len(positives)
+
+    negatives.sort(reverse=True)
+
+    for num in negatives:
+        if total_sum + num >= 0:
+            total_sum += num
+            count += 1
+        else:
+            break
+
+    return count
+
+
+
+
+
+
+t = int(input())
+for _ in range(t):
+    N = int(input())
+    Array = list(map(int, input().split()))
+
+    print(maximum_subsequence(Array))
