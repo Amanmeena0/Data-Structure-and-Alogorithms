864321475e8acf06b30bdfd507e8a501b67013a4
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-16 05:51:31 +0530

    Implement initial structure for merging intervals functionality

---
+++ Striver/Unit - 3 Arrays/Hard/MergeIntervals.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        return
+
+
+        
+def main():
+    sol = Solution()
+    array = list(map(int, input("Enter numbers separated by spaces: ").split()))
+    k = int(input("Enter the integer: "))
+    print(sol.merge(array,k))
+
+    
+if __name__ == "__main__":
+    main()
