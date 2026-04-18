afcda6f34a944a37fa23ce40e0c4fb7a656a5fae
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-18 17:56:04 +0530

    commit

--- Striver/Unit - 3 Arrays/Hard/9.FindTheReaptingAndMissingNumber.py
+++ Striver/Unit - 3 Arrays/Hard/9.FindTheReaptingAndMissingNumber.py
@@ -1,6 +1,8 @@
 from typing import List
 from collections import Counter
 class Solution:
+
+    # for the 1D array - hash approach
     def findMissingRepeatingNumbers(self, nums):
         n = len(nums)
         freq = Counter(nums)
@@ -16,7 +18,45 @@ class Solution:
                 repeating = i
             
         return [repeating, missing]
+    
+    # For the 2D array - hash approach
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         
+        n = len(grid)
+        freq = {}
+        
+        for row in grid:
+
+            for num in row:
+                freq[num] = freq.get(num, 0) + 1
+            
+        for num in range(1, n*n + 1):
+            if num not in freq:
+                missing = num 
+            elif freq[num] == 2:
+
+                repeat = num
+        return [repeat, missing]
+    
+    #For the 2D array using the maths 
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+
+        n = len(grid)
+        total = n * n
+
+        sum_val = sum(num for row in grid for num in row)
+        sqr_sum = sum(num*num for row in grid for num in row)
+
+        sum_diff = sum_val - total * (total +1 )// 2
+        sqr_diff = sqr_sum - total *(total + 1) * (2 * total + 1)//6
+
+
+        repeat = (sqr_diff // sum_diff + sum_diff) // 2
+        missing = (sqr_diff // sum_diff - -sum_diff) // 2
+
+        return [repeat, missing]
+
+
 def main():
     sol = Solution()
     array = list(map(int, input("Enter numbers separated by spaces: ").split()))
