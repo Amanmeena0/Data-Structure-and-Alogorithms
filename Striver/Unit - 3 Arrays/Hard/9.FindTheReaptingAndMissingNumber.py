01c98ccbc621eda138b540fe90ed99f8f6020edd
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-18 16:27:09 +0530

    message

---
+++ Striver/Unit - 3 Arrays/Hard/9.FindTheReaptingAndMissingNumber.py
@@ -0,0 +1,20 @@
+from typing import List
+from collections import Counter
+class Solution:
+    def findMissingRepeatingNumbers(self, nums):
+        Counter()
+        return
+        
+def main():
+    sol = Solution()
+    array = list(map(int, input("Enter numbers separated by spaces: ").split()))
+    # array1 = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+    print(sol.findMissingRepeatingNumbers(array))
+
+    # for num in array:
+    #     print(num)
+
+    
+if __name__ == "__main__":
+    main()
