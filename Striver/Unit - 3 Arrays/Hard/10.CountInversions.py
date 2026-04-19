3de4fdfdbf4d48645a1e40bac8312388750c65df
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-19 23:30:19 +0530

    Commit

--- Striver/Unit - 3 Arrays/Hard/10.CountInversions.py
+++ Striver/Unit - 3 Arrays/Hard/10.CountInversions.py
@@ -1,14 +1,74 @@
 from typing import List
 
 class Solution:
-    def numberOfInversions(self, nums: list[int], target:int) -> list[list[int]]:
+    # 1D - Array
+    # brute force approach
+    # def numberOfInversions1(self, nums: list[int]) -> int:
         
+    #     Inversion = 0
+    #     n = len(nums)
+    #     for i in range(i):
 
+    #         for j in range(i + 1):
+
+    #             if nums[j] > nums[i]:
+
+    #                 Inversion += 1
+ 
+    #     return Inversion
+    
+    # 1D - Array 
+    # Merge Sort Based - Optimal Approach
+    def merge(self, arr, low , mid , high):
+        temp = []
+
+        left = low 
+        right = mid + 1
+
+        cnt = 0
+
+        while left <= mid and right <= high:
+
+            if arr[left] <= arr[right]:
+                temp.append(arr[left])
+                left += 1
+            else:
+                temp.append(arr[right])
+                cnt += (mid - left + 1)
+                right +=1 
+
+        while left <= mid:
+            temp.append(arr[left])
+            left += 1
+
+        while right <= high:
+            temp.append(arr[right])
+            right += 1
         
+        for i in range(low, high + 1):
+            arr[i] = temp[i - low]
 
+        return cnt
 
+    def mergeSort(self, arr, low , high):
+        cnt = 0
 
-        return 
+        if low >= high:
+            return cnt 
+        
+        mid = (low + high) // 2
+
+        cnt += self.mergeSort(arr, low, mid)
+
+        cnt += self.mergeSort(arr, mid + 1, high)
+
+        cnt += self.merge(arr, low, mid, high)
+
+        return cnt
+    
+    def numberOfInversions(self, nums: list[int]) -> int:
+        return self.mergeSort(nums, 0, len(nums) - 1)
+        
 def main():
 
     sol = Solution()
