e7dafb31673e81a8cbda7540daff180962abc2c1
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-19 21:43:37 +0530

    commit

---
+++ Striver/Unit - 3 Arrays/Hard/10.CountInversions.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def numberOfInversions(self, nums: list[int], target:int) -> list[list[int]]:
+        
+        
+
+
+        return 
+def main():
+
+    sol = Solution()
+    array = list(map(int, input("Enter numbers separated by spaces: ").split()))
+    # k = int(input("Enter the number: "))
+    print(sol.numberOfInversions(array))
+    # sol.sortColors(array)
+    # for i in array:
+    #     print(i, end=" ")
+
+if __name__ == "__main__":
+    main()
