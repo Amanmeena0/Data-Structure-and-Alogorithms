0625c67c425b1755d863d7c5a818b100b8f2c450
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-19 23:31:31 +0530

    commit

---
+++ Striver/Unit - 3 Arrays/Hard/11.ReversePairs.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+
+
+        return 
+def main():
+
+    sol = Solution()
+    array = list(map(int, input("Enter numbers separated by spaces: ").split()))
+    # k = int(input("Enter the number: "))
+    print(sol.reversePairs(array))
+    # sol.sortColors(array)
+    # for i in array:
+    #     print(i, end=" ")
+
+if __name__ == "__main__":
+    main()
