4e6f7a35365f40cd33e2d40d125928d537afb68a
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-20 22:31:16 +0530

    commit

--- Striver/Unit - 3 Arrays/Hard/11.ReversePairs.py
+++ Striver/Unit - 3 Arrays/Hard/11.ReversePairs.py
@@ -2,9 +2,47 @@ from typing import List
 
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
+        res = [0]
 
+        def merge(nums):
+            
+            n = len(nums)
 
-        return 
+            mid = n // 2
+
+            if n == 1: return nums
+
+            arrLeft = merge(nums[:mid])
+            arrRight = merge(nums[mid:])
+
+            nLeft, nRight =  mid, n - mid
+
+            i, j = 0,0
+
+            while i < nLeft:
+                while j < nRight and arrLeft[i] > 2 * arrRight[j]: j += 1
+                res[0] += j
+                i += 1
+
+            i, j = 0,0
+            sortedArr = []
+
+            while i < nLeft and j < nRight:
+                if arrLeft[i] < arrRight[j]:
+                    sortedArr.append(arrLeft[i])
+                    i += 1
+                else:
+                    sortedArr.append(arrRight[j])
+                    j += 1
+
+            sortedArr.extend(arrLeft[i:])
+            sortedArr.extend(arrRight[j:])
+
+            return sortedArr
+        
+        merge(nums)
+
+        return res[0]
 def main():
 
     sol = Solution()
