42bb6636b40d666ce6f1248219adac9418b06a17
AMAN MEENA <141953689+Amanmeena0@users.noreply.github.com>
2026-04-28 08:06:39 +0530

    commit changes

--- Striver/Unit - 4 Binary Search/BS on 1D Array/4.searchForInsertPositions.py
+++ Striver/Unit - 4 Binary Search/BS on 1D Array/4.searchForInsertPositions.py
@@ -5,19 +5,18 @@ class Solution:
         
         low = 0
         high = len(nums) - 1
-        ans = 0
+        
         while low <= high:
 
             mid = (low + high) // 2
 
-            if nums[mid] <= target:
-                low = mid + 1
+            if nums[mid] == target:
+                return mid
             elif nums[mid] > target:
-                ans = mid
                 high = mid-1
-            
-
-        return ans
+            elif nums[mid] < target:
+                low = mid + 1 
+        return low
 
 
 def main():
